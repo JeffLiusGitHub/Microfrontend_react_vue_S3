cf1ff2ccf533d90a31043a0ce249604199e4cb10
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2023-01-09 23:30:12 +0930

    add prefix to production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 const App = () => {
+	const generateClassName = createGenerateClassName({
+		productionPrefix: 'container',
+	});
 	return (
 		<BrowserRouter>
-			<Header />
-			<MarketingApp />
+			<StylesProvider generateClassName={generateClassName}>
+				<Header />
+				<MarketingApp />
+			</StylesProvider>
 		</BrowserRouter>
 	);
 };
